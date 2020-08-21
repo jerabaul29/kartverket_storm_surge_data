6d7c0820bc0278b1a485cb6739666f391663e335
JRT <jean.rblt@gmail.com>
2020-08-21 12:25:23 +0200

    Fix dataset generation script

--- kartverket_stormsurge/script_generate_nc4_dataset.py
+++ kartverket_stormsurge/script_generate_nc4_dataset.py
@@ -10,9 +10,10 @@ example_kind = "full"
 
 if example_kind == "full":
     start = datetime.datetime(1970, 1, 1, 0, 0, tzinfo=pytz.utc)
-    end = datetime.datetime(2020, 1, 1, 0, 0, tzinfo=pytz.utc)
+    end = datetime.datetime(2020, 6, 1, 0, 0, tzinfo=pytz.utc)
 
     path_to_nc4 = "./full_size_data_kartverket_storm_surge.nc4"
+
 elif example_kind == "short":
     start = datetime.datetime(2008, 1, 1, 0, 0, tzinfo=pytz.utc)
     end = datetime.datetime(2008, 6, 1, 0, 0, tzinfo=pytz.utc)
@@ -20,10 +21,5 @@ elif example_kind == "short":
     path_to_nc4 = "./small_size_data_kartverket_storm_surge.nc4"
 else:
     raise ValueError("unknown example_kind")
-
-start = datetime.datetime(2008, 1, 1, 0, 0, tzinfo=pytz.utc)
-end = datetime.datetime(2008, 6, 1, 0, 0, tzinfo=pytz.utc)
-
-path_to_nc4 = "./small_size_data_kartverket_storm_surge.nc4"
 
 dataset_generator.generate_netCDF4_dataset(start, end, nc4_path=path_to_nc4)
